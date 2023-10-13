f7850484f29901eac06abd398b7e88f24cfd6050
Stephen Crocker <stephen.crocker@pallas.uk>
2023-10-13 10:00:50 +0100

    Implemented a router decorator for the storybook view so I could see the banner nar storybook tests.
    Implement tests for long words and multiple word to demonstrate the change works

--- src/components/BannerNavBar/BannerNavBar.stories.tsx
+++ src/components/BannerNavBar/BannerNavBar.stories.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
 import { BannerNavBar } from './BannerNavBar.components';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
-export default { component: BannerNavBar };
+export default {
+  component: BannerNavBar,
+  decorators: [
+    (Story) => (
+      <HashRouter>
+        <Routes>
+          <Route path={''} element={<Story />} />
+          <Route path={'/services'} element={<Story />} />
+          <Route path={'/team'} element={<Story />} />
+        </Routes>
+      </HashRouter>
+    ),
+  ],
+};
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <BannerNavBar {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {}
+Default.args = {};
 
 export const Complete = Template.bind({});
 Complete.args = {
-  brandName: "Pallas Systems",
-  brandImg: { src: "./images/logos/logo.svg"},
+  brandName: 'Pallas Systems',
+  brandImg: { src: './images/logos/logo.svg' },
   items: [
-      {text: "Home", path: ""},
-      {text: "Services", path: "/services"},
-      {text: "Team", path: "/team"}
-  ]
+    { text: 'Home', path: '' },
+    { text: 'Services', path: '/services' },
+    { text: 'Team', path: '/team' },
+  ],
+};
+
+export const SpacedWords = Template.bind({});
+SpacedWords.args = {
+  brandName: 'Pallas Systems',
+  items: [
+    { text: 'Getting Started', path: '' },
+    { text: 'User Guide', path: '/services' },
+    { text: 'Flying Wing', path: '/team' },
+  ],
+};
+
+export const LongWords = Template.bind({});
+LongWords.args = {
+  brandName: 'Pallas Systems',
+  items: [
+    { text: 'GettingStartedWithTomAndDick', path: '' },
+    { text: 'User', path: '/services' },
+    { text: 'Supercalifragilisticexpialidocious', path: '/services' },
+  ],
 };
