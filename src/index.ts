beb5c3d656be0f2baff9ba9e64be4f95fc41fc0e
Stephen Crocker <stephen.crocker@pallas.uk>
2023-11-13 23:14:31 +0100

    Removed default data from the index

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ export { PallasSVG } from './Common/index';
 export type { ImageProperty } from './Common/index';
 
 // All of these are linked to the Header for the page
-export { BannerNavBar, BannerNavBarDefaultData } from './BannerNavBar/index';
+export { BannerNavBar } from './BannerNavBar/index';
 export type { NavbarProperty, NavbarLinkProperty } from './BannerNavBar/index';
 
 // All of these are linked to the Footer, which is used on multiple pages.
-export { Footer, FooterDefaultData } from './Footer/index';
+export { Footer } from './Footer/index';
 export type { FooterProperties } from './Footer/index';
 
 export { ContactSection } from './ContactSection/index';
