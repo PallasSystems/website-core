5684711bdc15eb30274bfe68c6967e9ec08648ab
Stephen Crocker <stephen.crocker@pallas.uk>
2024-01-21 15:13:03 +0100

    Fixed the test

--- src/SCMIcon/SCMIcon.test.tsx
+++ src/SCMIcon/SCMIcon.test.tsx
@@ -5,22 +5,13 @@ import '@testing-library/jest-dom';
 import { SCMIcon } from './SCMIcon.components';
 
 test('Valid GitHub Credentials', () => {
-  const renderedResult = render(<SCMIcon project='proj' repository='repo' />);
+  render(<SCMIcon project='proj' repository='repo' />);
 
-  const link = renderedResult.findByRole('link');
-  expect(link).toHaveAttribute('href', 'https://github.com/proj/repo');
-});
-
-test('Blank GitHub Credentials', () => {
-  const renderedResult = render(<SCMIcon project='' repository='' />);
-
-  const link = renderedResult.findByRole('link');
-  expect(link).toHaveAttribute('href', '');
+  expect(screen.getByRole('link')).toHaveAttribute('href', 'https://github.com/proj/repo');
 });
 
 test('Valid Alternative Credentials', () => {
-  const renderedResult = render(<SCMIcon project='proj' repository='repo' endpoint='https://bitbucket.org/' />);
+  render(<SCMIcon project='proj' repository='repo' endpoint='https://bitbucket.org/' />);
 
-  const link = renderedResult.findByRole('link');
-  expect(link).toHaveAttribute('href', 'https://bitbucket.org/proj/repo');
+  expect(screen.getByRole('link')).toHaveAttribute('href', 'https://bitbucket.org/proj/repo');
 });
