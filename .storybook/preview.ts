826e0554db29f5ce892047dd839b7dc0dcee9539
Stephen Crocker <stephen.crocker@pallas.uk>
2024-02-03 10:13:03 +0100

    Modified preview so it pulls in the css we provide to services

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,9 @@
 import type { Preview } from '@storybook/react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../src/css/base.css';
+import '../src/css/bootstrap.solar.css';
+import '../src/css/bootstrap.pallas.css';
 
 const preview: Preview = {
   parameters: {
