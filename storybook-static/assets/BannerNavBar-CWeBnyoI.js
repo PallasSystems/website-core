0dd7447493ddf479f47a5324556c70450244fff3
pallasCI <continuous.integration@pallas.uk>
2024-05-07 22:03:33 +0000

    Updates

---
+++ storybook-static/assets/BannerNavBar-CWeBnyoI.js
@@ -0,0 +1,10 @@
+import{j as n}from"./Container-DwgHVSFH.js";import{useMDXComponents as i}from"./index-DlpNa54Y.js";import{M as r,C as o}from"./index-BnoavI2l.js";import{B as s,M as l,a as c,C as p}from"./BannerNavBar.stories-DsFK07u-.js";import"./_commonjsHelpers-BosuxZz1.js";import"./index-BwDkhjyp.js";import"./iframe-B1pWno2q.js";import"../sb-preview/runtime.js";import"./index-CReuRBEY.js";import"./inheritsLoose-BvXrZCDk.js";import"./index-DXimoRZY.js";import"./index-B8K4vdXH.js";import"./index-DrFu-skq.js";import"./index-Dk74W0Oi.js";import"./SCMIcon.components-CjtqQImL.js";function e(t){const a={h1:"h1",p:"p",...i(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(r,{of:s}),`
+`,n.jsx(a.h1,{id:"branding-only-navbar",children:"Branding Only NavBar"}),`
+`,n.jsx(a.p,{children:"The Navigation Bar expects a brand name and will produce a basic Bootstrap NavBar object to display this."}),`
+`,n.jsx(o,{of:l}),`
+`,n.jsx(a.h1,{id:"basic-navbar",children:"Basic NavBar"}),`
+`,n.jsx(a.p,{children:"It is expected that any tool built using this framework will consist of multiple pages and so the Navigational bar will include a second row holding links to these pages. Currently the links expect to use the React Router component to generate."}),`
+`,n.jsx(o,{of:c}),`
+`,n.jsx(a.h1,{id:"standard-navbar",children:"Standard NavBar"}),`
+`,n.jsx(a.p,{children:"Typically a tool should contain the link to the source code, a branding icon, the tool name and a selection of sub pages the brand bar is associated with, as shown below."}),`
+`,n.jsx(o,{of:p})]})}function N(t={}){const{wrapper:a}={...i(),...t.components};return a?n.jsx(a,{...t,children:n.jsx(e,{...t})}):e(t)}export{N as default};
