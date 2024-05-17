4d0085525c5309436ac80bab328522e12c24251b
Stephen Crocker <stephen.crocker@pallas.uk>
2024-05-17 16:29:27 +0100

    Changes to support storybook within docs itself

---
+++ docs/.storybook/preview.tsx
@@ -0,0 +1,24 @@
+import { Preview } from '@storybook/react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../src/css/base.css';
+import '../../src/css/bootstrap.solar.css';
+import '../../src/css/bootstrap.pallas.css';
+
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      expanded: true,
+      hideNoControlsWarning: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
