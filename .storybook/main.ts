0012c473cc27c2f92ce0876fc9225d5068468537
Stephen Crocker <stephen.crocker@pallas.uk>
2024-10-14 22:42:04 +0100

    Updates to the latest form of libraries

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,14 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-  framework: '@storybook/react-vite',
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-webpack5-compiler-swc'
+  ],
+  framework: '@storybook/react-webpack5',
   docs: {
     autodocs: 'tag',
     defaultName: 'Documentation',
--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,14 +1,9 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 
-import { ExamplesHeadPage, ExamplesSectionPage, ExamplesStorybookPage } from '@pallassystems/website-core';
 import type { NavbarLinkProperty } from '@pallassystems/website-core';
 
 import {
-  ApiHeadPage,
-  ApiBannerNavBarPageData,
-  GettingStartedInstallPage,
-  GettingStartedPage,
-  GettingStartedUsagePage,
+
   HomePage,
 } from './pages';
 import { ExamplePageData, PageData } from './App.data';
@@ -25,20 +20,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path={''} element={<HomePage {...PageData} />} />
-        <Route path={'/api'} element={<ApiHeadPage {...PageData} />} />
-        {ApiBannerNavBarPageData.map((value) => {
-          return <Route path={value.link} element={value.page(PageData)} />;
-        })}
-        <Route path={'/examples'} element={<ExamplesHeadPage {...PageData} exampleProps={ExamplePageData} />} />
-        {ExamplePageData.map((value: NavbarLinkProperty) => {
-          return <Route path={value.path} element={<ExamplesSectionPage exampleProps={value} {...PageData} />} />;
-        })}
-        {storybooks.map((value: NavbarLinkProperty) => {
-          return <Route path={value.path} element={<ExamplesStorybookPage exampleProps={value} {...PageData} />} />;
-        })}
-        <Route path={'/gettingstarted'} element={<GettingStartedPage {...PageData} />} />
-        <Route path={'/gettingstarted/install'} element={<GettingStartedInstallPage {...PageData} />} />
-        <Route path={'/gettingstarted/usage'} element={<GettingStartedUsagePage {...PageData} />} />
+        
       </Routes>
     </HashRouter>
   );
